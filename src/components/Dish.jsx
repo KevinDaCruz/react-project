1393a37efa7fae4aac62cf093a742f2f6bdf612d
Kevin Da Cruz <kevindacruz@MacBookAir.lan>
2025-05-19 14:01:20 +0200

    Activité 6: Corrections

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,9 +1,9 @@
 export default function Dish({ name, price, image }) {
   return (
-    <main>
+    <>
       <h3>{name}</h3>
       <img src={image} alt={name} />
-      <p>{price}</p>
-    </main>
+      <p>{price}€</p>
+    </>
   );
 }
