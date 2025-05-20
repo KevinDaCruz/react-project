3d0a2c0528521cca69d98e4854cef773adb5d9ad
Kevin Da Cruz <kevindacruz@host-87-20-168-192.retail.telecomitalia.it>
2025-05-20 11:26:59 +0200

    Activité 10: Ajout d'un tableau dishes et boucle sur le tableau pour l'affichage dans App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,46 @@ import "./assets/styles/App.scss";
 import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
+  const dishes = [
+    {
+      name: "Tacos à l’unité",
+      price: 3,
+      image:
+        "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      isNew: true,
+    },
+    {
+      name: "Enchiladas",
+      price: 12,
+      image:
+        "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      isNew: false,
+    },
+    {
+      name: "Mole poblano",
+      price: 15,
+      image:
+        "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      isNew: false,
+    },
+  ];
+
   return (
     <>
       <Header />
       <main>
         <Container>
           <Row className="gx-3">
-            <Col xs={12} md={4}>
-              <Dish
-                name="Tacos à l’unité"
-                price={3}
-                image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                isNew ={true}
-              />
-            </Col>
-            <Col xs={12} md={4}>
-              <Dish
-                name="Enchiladas"
-                price={12}
-                image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                isNew={false}
-              />
-            </Col>
-            <Col xs={12} md={4}>
-              <Dish
-                name="Mole poblano"
-                price={15}
-                image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                isNew={false}
-              />
-            </Col>
+            {dishes.map((dish, index) => (
+              <Col xs={12} md={4} key={index}>
+                <Dish
+                  name={dish.name}
+                  price={dish.price}
+                  image={dish.image}
+                  isNew={dish.isNew}
+                />
+              </Col>
+            ))}
           </Row>
         </Container>
       </main>
