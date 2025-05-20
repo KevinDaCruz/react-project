776b5e94c3c85717e10eb2e5ecbc76d7b8486909
Kevin Da Cruz <kevindacruz@host-87-20-168-192.retail.telecomitalia.it>
2025-05-20 11:45:29 +0200

    Activité 11: Filtre sur le tableau dishes pour stock supérieur à 0

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       image:
         "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
       isNew: true,
+      stock: 12,
     },
     {
       name: "Enchiladas",
@@ -22,6 +23,7 @@ function App() {
       image:
         "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
       isNew: false,
+      stock: 0,
     },
     {
       name: "Mole poblano",
@@ -29,8 +31,11 @@ function App() {
       image:
         "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
       isNew: false,
+      stock: 5,
     },
   ];
+
+  const filteredDishes = dishes.filter((dish) => dish.stock > 0);
 
   return (
     <>
@@ -38,7 +43,7 @@ function App() {
       <main>
         <Container>
           <Row className="gx-3">
-            {dishes.map((dish, index) => (
+            {filteredDishes.map((dish, index) => (
               <Col xs={12} md={4} key={index}>
                 <Dish
                   name={dish.name}
